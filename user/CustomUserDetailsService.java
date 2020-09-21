af7613ba4101f220c0302e85eaa4f569ef8c836c
jackwashere <jerome.goh.2019@sis.smu.edu.sg>
2020-09-21 16:11:31 +0800

    Create CustomUserDetailsService.java
    added customUserDetailService

---
+++ user/CustomUserDetailsService.java
@@ -0,0 +1,19 @@
+package blah.blah;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository users;
+    
+    public CustomUserDetailsService(UserRepository users) {
+        this.users = users;
+    }
+    public UserDetails loadUserByUsername(String username)  throws UsernameNotFoundException {
+        return users.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User '" + username + "' not found"));
+    }
+    
+}
