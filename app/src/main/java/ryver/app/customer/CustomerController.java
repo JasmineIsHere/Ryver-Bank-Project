6a41e7a4d18c7b550e2f984b55b6bd52c532c920
jackwashere <jerome.goh.2019@sis.smu.edu.sg>
2020-10-03 11:32:30 +0800

    validateNric

--- app/src/main/java/ryver/app/customer/CustomerController.java
+++ app/src/main/java/ryver/app/customer/CustomerController.java
@@ -63,7 +63,127 @@ public class CustomerController {
     @PostMapping("/customers")
     public Customer addCustomer(@Valid @RequestBody Customer customer){
         customer.setPassword(encoder.encode(customer.getPassword()));
-        return customers.save(customer);
+        if (validateNric(customer.getNric()))
+            return customers.save(customer);
+        else
+            return false;
     }
+
+    public static boolean validateNric(String nric){
+        int total = 0;
+        int arr[] = {2,7,6,5,4,3,2};
+        char charArr[] = nric.toCharArray();
+        
+        if(nric.length()-2 != arr.length)
+            return false;
+        
+            for(int i=0; i<arr.length; i++){
+                total += arr[i] * Character.getNumericValue(charArr[i+1]);
+            }
+        
+        if(charArr[0] == 'T' || charArr[0] == 'G')
+            total += 4;
+        
+        if(charArr[0] == 'S' || charArr[0] == 'T'){
+            switch(total%11) {
+            case 0:
+              if(charArr[charArr.length-1] == 'J')
+                  return true;
+              break;
+            case 1:
+              if(charArr[charArr.length-1] == 'Z')
+                  return true;
+              break;
+            case 2:
+              if(charArr[charArr.length-1] == 'I')
+                  return true;
+              break;
+            case 3:
+              if(charArr[charArr.length-1] == 'H')
+                  return true;
+              break;
+            case 4:
+              if(charArr[charArr.length-1] == 'G')
+                  return true;
+              break;
+            case 5:
+              if(charArr[charArr.length-1] == 'F')
+                  return true;
+              break;
+            case 6:
+              if(charArr[charArr.length-1] == 'E')
+                  return true;
+              break;
+            case 7:
+              if(charArr[charArr.length-1] == 'D')
+                  return true;
+              break;
+            case 8:
+              if(charArr[charArr.length-1] == 'C')
+                  return true;
+              break;
+            case 9:
+              if(charArr[charArr.length-1] == 'B')
+                  return true;
+              break;
+            case 10:
+              if(charArr[charArr.length-1] == 'A')
+                  return true;
+              break;
+            default:
+              return false;
+          }
+        }else if(charArr[0] == 'F' || charArr[0] == 'G'){
+            switch(total%11) {
+            case 0:
+              if(charArr[charArr.length-1] == 'X')
+                  return true;
+              break;
+            case 1:
+              if(charArr[charArr.length-1] == 'W')
+                  return true;
+              break;
+            case 2:
+              if(charArr[charArr.length-1] == 'U')
+                  return true;
+              break;
+            case 3:
+              if(charArr[charArr.length-1] == 'T')
+                  return true;
+              break;
+            case 4:
+              if(charArr[charArr.length-1] == 'R')
+                  return true;
+              break;
+            case 5:
+              if(charArr[charArr.length-1] == 'Q')
+                  return true;
+              break;
+            case 6:
+              if(charArr[charArr.length-1] == 'P')
+                  return true;
+              break;
+            case 7:
+              if(charArr[charArr.length-1] == 'N')
+                  return true;
+              break;
+            case 8:
+              if(charArr[charArr.length-1] == 'M')
+                  return true;
+              break;
+            case 9:
+              if(charArr[charArr.length-1] == 'L')
+                  return true;
+              break;
+            case 10:
+              if(charArr[charArr.length-1] == 'K')
+                  return true;
+              break;
+            default:
+              return false;
+            }
+        }
+        return false;
+       }
    
 }
