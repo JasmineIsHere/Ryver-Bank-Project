5ae1dc1239c543edafc6aff166609f9b29b03cb9
Jen Leng <jen.leng.2019@sis.smu.edu.sg>
2020-10-10 10:37:37 +0800

    Update TransactionRepository.java

--- app/src/main/java/ryver/app/transaction/TransactionRepository.java
+++ app/src/main/java/ryver/app/transaction/TransactionRepository.java
@@ -3,7 +3,9 @@ package ryver.app.transaction;
 import java.util.*;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long>{
     // additional derived queries specified here will be implemented by Spring Data JPA
     // start the derived query with "findBy", then reference the entity attributes you want to filter
