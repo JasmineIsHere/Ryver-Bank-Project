a83ff0c94c658bcd5d6050b7f2809afe1b42ff4e
jolene loh <jolene.loh.2019@sis.smu.edu.sg>
2020-10-12 21:57:52 +0800

    junit v3.2
    -forgot to command off for the team whoops

--- app/src/test/java/ryver/app/content/ContentServiceTest.java
+++ app/src/test/java/ryver/app/content/ContentServiceTest.java
@@ -1,54 +1,52 @@
-package test.java.ryver.app;
+// package test.java.ryver.app;
 
-import ryver.app.customer.*;
-import ryver.app.account.*;
-import ryver.app.customer.Customer.*;
-import ryver.app.account.Account.*;
+// import ryver.app.customer.*;
+// import ryver.app.account.*;
+// import ryver.app.customer.Customer.*;
+// import ryver.app.account.Account.*;
 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.ApplicationContext;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertNotNull;
+// import static org.junit.jupiter.api.Assertions.assertNull;
+// import static org.mockito.ArgumentMatchers.any;
+// import static org.mockito.Mockito.verify;
+// import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.mockito.InjectMocks;
+// import org.mockito.Mock;
+// import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
-public class ContentServiceTest {
-    @Mock
-    private ContentRepository contents;
-    // findByCustomerId(Long customerId);
-    // findByIdAndCustomerId(Long accountId, Long customerId);
+// @ExtendWith(MockitoExtension.class)
+// public class ContentServiceTest {
+//     @Mock
+//     private ContentRepository contents;
+//     // List<Content> findByApproved(boolean approved);
 
-    @Mock
-    BCryptPasswordEncoder encoder;
+//     @Mock
+//     BCryptPasswordEncoder encoder;
 
-    @InjectMocks
-    private ContentController ContentController;
+//     @InjectMocks
+//     private ContentController ContentController;
 
-    void addContent_approveContent_returnSavedContent(){}
-    void updateContent_ROLEManager_approveContentAndReturnSavedContent(){}
-    void updateContent_ROLEAnalyst_returnSavedContent(){}
-    void updateContent_invalidContentId_returnException(){}
-    // public Content addContent(@Valid @RequestBody Content content) {
-    //     public Content updateContent(@PathVariable (value = "contentId") Long contentId, @Valid @RequestBody Content updatedContentInfo) {
-    //         public void deleteContent(@PathVariable (value = "contentId") Long contentId) {
+//     void addContent_approveContent_returnSavedContent(){}
+//     void updateContent_ROLEManager_approveContentAndReturnSavedContent(){}
+//     void updateContent_ROLEAnalyst_returnSavedContent(){}
+//     void updateContent_invalidContentId_returnException(){}
+//     // public Content addContent(@Valid @RequestBody Content content) {
+//     // public Content updateContent(@PathVariable (value = "contentId") Long contentId, @Valid @RequestBody Content updatedContentInfo) {
 
 
 
-}
+// }
--- app/src/test/java/ryver/app/transaction/TransactionServiceTest.java
+++ app/src/test/java/ryver/app/transaction/TransactionServiceTest.java
@@ -1,55 +1,55 @@
-package test.java.ryver.app;
+// package test.java.ryver.app;
 
-import ryver.app.customer.*;
-import ryver.app.account.*;
-import ryver.app.customer.Customer.*;
-import ryver.app.account.Account.*;
+// import ryver.app.customer.*;
+// import ryver.app.account.*;
+// import ryver.app.customer.Customer.*;
+// import ryver.app.account.Account.*;
 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.ApplicationContext;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertNotNull;
+// import static org.junit.jupiter.api.Assertions.assertNull;
+// import static org.mockito.ArgumentMatchers.any;
+// import static org.mockito.Mockito.verify;
+// import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.mockito.InjectMocks;
+// import org.mockito.Mock;
+// import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
-public class TransactionServiceTest {
-    @Mock
-    private TransactionRepository transactions;
-    // findByCustomerId(Long customerId);
-    // findByIdAndCustomerId(Long accountId, Long customerId);
+// @ExtendWith(MockitoExtension.class)
+// public class TransactionServiceTest {
+//     @Mock
+//     private TransactionRepository transactions;
+//     // List<Transaction> findByAccountId(Long accountId);
+//     // List<Transaction> findBySenderOrReceiver(Long accountId, Long accountId2);
 
-    @Mock
-    BCryptPasswordEncoder encoder;
+//     @Mock
+//     BCryptPasswordEncoder encoder;
 
-    @InjectMocks
-    private TransactionController transactionController;
+//     @InjectMocks
+//     private TransactionController transactionController;
 
-    @Test
-    void getAllTransactionsByAccountId_getTransactionList_returnListOfTransactions(){}
-    void addTransaction_newTransaction_returnSavedTransaction(){}
-    void addTransaction_invalidAccountId_returnException(){}
-    void addTransaction_badBalance_returnException(){}
-    void addTransaction_insufficientBalance_returnException(){}
+//     @Test
+//     void getAllTransactionsByAccountId_getTransactionList_returnListOfTransactions(){}
+//     void addTransaction_newTransaction_returnSavedTransaction(){}
+//     void addTransaction_invalidAccountId_returnException(){}
+//     void addTransaction_badBalance_returnException(){}
+//     void addTransaction_insufficientBalance_returnException(){}
 
 
-    // public List<Transaction> getAllTransactionsByAccountId(@PathVariable (value = "accountId") Long accountId) {
-    // public Transaction addTransaction (@PathVariable (value = "accountId") Long accountId, @Valid @RequestBody Transaction transaction) {
+//     // public List<Transaction> getAllTransactionsByAccountId(@PathVariable (value = "accountId") Long accountId) {
+//     // public Transaction addTransaction (@PathVariable (value = "accountId") Long accountId, @Valid @RequestBody Transaction transaction) {
 
-}
+// }
