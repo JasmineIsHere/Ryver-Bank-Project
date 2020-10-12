7031799edd6cc518e1844a40d20861ef5e8d2fa2
jolene loh <jolene.loh.2019@sis.smu.edu.sg>
2020-10-12 21:36:04 +0800

    junit v3
    -creation of Transaction and Content Service tests

---
+++ app/src/test/java/ryver/app/transaction/TransactionServiceTest.java
@@ -0,0 +1,44 @@
+package test.java.ryver.app;
+
+import ryver.app.customer.*;
+import ryver.app.account.*;
+import ryver.app.customer.Customer.*;
+import ryver.app.account.Account.*;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TransactionServiceTest {
+    @Mock
+    private TransactionRepository transactions;
+    // findByCustomerId(Long customerId);
+    // findByIdAndCustomerId(Long accountId, Long customerId);
+
+    @Mock
+    BCryptPasswordEncoder encoder;
+
+    @InjectMocks
+    private TransactionController transactionController;
+
+}
