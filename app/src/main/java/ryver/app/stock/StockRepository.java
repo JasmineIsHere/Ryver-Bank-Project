bdf8a46a7cd952f2f8b93254c3cdf83a6083fb0d
Jen Leng <jen.leng.2019@sis.smu.edu.sg>
2020-10-13 13:04:28 +0800

    Minor changes to stock and trade repositories

--- app/src/main/java/ryver/app/stock/StockRepository.java
+++ app/src/main/java/ryver/app/stock/StockRepository.java
@@ -1,8 +1,9 @@
 // package ryver.app.stock;
 
-
 // import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
 
+// @Repository
 // public interface StockRepository extends JpaRepository<CustomStock, String>{
-
+    
 // }
--- app/src/main/java/ryver/app/trade/TradeRepository.java
+++ app/src/main/java/ryver/app/trade/TradeRepository.java
@@ -1,8 +1,9 @@
 // package ryver.app.trade;
 
-
 // import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
 
+// @Repository
 // public interface TradeRepository extends JpaRepository<Trade, Long>{
-
+    
 // }
