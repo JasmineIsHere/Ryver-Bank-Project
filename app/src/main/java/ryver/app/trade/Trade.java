2a8de59148fccc014429436f41125c20b718c4aa
See Ke Xin <kexin.see.2019@sis.smu.edu.sg>
2020-10-16 21:14:50 +0800

    trade and trade repo

--- app/src/main/java/ryver/app/trade/Trade.java
+++ app/src/main/java/ryver/app/trade/Trade.java
@@ -1,41 +1,70 @@
-// package ryver.app.trade;
+package ryver.app.trade;
 
-// import ryver.app.account.*;
-// import ryver.app.stock.*;
-
-// import javax.persistence.*;
-// import javax.validation.constraints.*;
+import ryver.app.account.*;
+import ryver.app.stock.*;
 
 
-// import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.*;
 
-// @Entity
-// @Getter
-// @Setter
-// @ToString
-// @AllArgsConstructor
-// @NoArgsConstructor
-// @EqualsAndHashCode
-// public class Trade {
-//     private  @Id @GeneratedValue (strategy = GenerationType.IDENTITY) Long id;
 
-//     private String action; //buy or sell
-//     private String symbol; //symbol of stock - stock.getSymbol()
-//     private int quantity; //in multiples of 100
-//     private double bid; //for buying
-//     private double ask; //for selling
-//     private double avg_price; 
-//     private int filled_quanity; //# of quantity successfully sold/bought out of total "quantity"
-//     private long timestamp; //Unix timestamp //Timestamp object somewhere //Timestamp.getTime()
-//     private String status; //open/filled/partial-filled/cancelled/expired
-//     private Long account_id; //account used to submit buy/sell request
-//     private Long customer_id;//customer submitting the buy/sell request
+import lombok.*;
 
-//     @ManyToOne
-//     @JoinColumn(name = "account", nullable = false)
-//     private Account account;
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Trade {
+    private  @Id @GeneratedValue (strategy = GenerationType.IDENTITY) Long id;
 
-//    // @ManyToOne
-//    // @JoinColumn(name = "stock_id", nullable = false)
-//    // private Stock stock;
-// }
+    @NotNull(message = "Action should not be null")
+    private String action; //buy or sell
+
+    @NotNull(message = "Symbol should not be null")
+    private String symbol; //symbol of stock - stock.getSymbol()
+
+
+    @NotNull(message = "Symbol should not be null. ")
+    private int quantity; //in multiples of 100
+
+    private double bid; //for buying
+    private double ask; //for selling
+    
+    private double avg_price; // the average filled price, as one trade can be matched by several other trades
+    private int filled_quanity; //# of quantity successfully sold/bought out of total "quantity"
+
+    private long date; //Unix timestamp 
+
+    // @NotNull(message = "Status should not be null. ")
+    private String status; //open/filled/partial-filled/cancelled/expired
+
+    // @NotNull(message = "Account ID should not be null. ")
+    @Column(name = "account_id")
+    private Long accountId; //account used to submit buy/sell request
+
+    // @NotNull(message = "Customer ID should not be null. ")
+    @Column(name = "customer_id")
+    private Long customerId;//customer submitting the buy/sell request
+
+    @ManyToOne
+    @JoinColumn(name = "account", nullable = false)
+    private Account account;
+
+    @ManyToOne
+    @JoinColumn(name = "stock_id", nullable = false)
+    private CustomStock stock;
+
+
+    public Trade(String action, String symbol, int quantity, double bid, double ask){
+        this.action = action;
+        this.symbol = symbol;
+        this.quantity = quantity;
+        this.bid = bid;
+        this.ask = ask;
+        // this.accountId = accountId;
+        // this.customerId = customerId;
+    }
+}
--- app/src/main/java/ryver/app/trade/TradeRepository.java
+++ app/src/main/java/ryver/app/trade/TradeRepository.java
@@ -1,9 +1,12 @@
-// package ryver.app.trade;
+package ryver.app.trade;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
+import java.util.*;
 
-// @Repository
-// public interface TradeRepository extends JpaRepository<Trade, Long>{
-    // List<Trade> findByCustomerId(Long customer_id);
-// }
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TradeRepository extends JpaRepository<Trade, Long>{
+    List<Trade> findByCustomerId(Long customerId);
+    Trade findByIdAndCustomerId(Long customerId, Long tradeId);
+}
