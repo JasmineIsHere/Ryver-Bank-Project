0cfd38b0b04118b22123587a43d86fc4862140a5
See Ke Xin <kexin.see.2019@sis.smu.edu.sg>
2020-10-21 14:46:09 +0800

    Update InsufficientStockException.java

--- app/src/main/java/ryver/app/trade/InsufficientStockException.java
+++ app/src/main/java/ryver/app/trade/InsufficientStockException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "There is insufficient stock in your portfolio")
-public class InsufficientStockException {
+public class InsufficientStockException extends RuntimeException{
     private static final long serialVersionUID = 1L;
 
     public InsufficientStockException() {
