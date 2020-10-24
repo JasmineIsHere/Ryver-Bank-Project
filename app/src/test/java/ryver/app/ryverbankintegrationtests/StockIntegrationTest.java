9bb12c0a8137d75ef97ba327f297a15ec9e085ff
Jasmine <jasmine.tan.2019@sis.smu.edu.sg>
2020-10-24 20:01:15 +0800

    Update StockIntegrationTest
    test
    - valid user, getStock OK

--- app/src/test/java/ryver/app/ryverbankintegrationtests/StockIntegrationTest.java
+++ app/src/test/java/ryver/app/ryverbankintegrationtests/StockIntegrationTest.java
@@ -1,83 +1,86 @@
-// package ryver.app.ryverbankintegrationtests;
+package ryver.app.ryverbankintegrationtests;
 
-// import ryver.app.stock.StockRepository;
-// import ryver.app.stock.CustomStock;
-// import ryver.app.customer.*;
+import ryver.app.stock.StockRepository;
+import ryver.app.trade.Trade;
+import ryver.app.customer.Customer;
+import ryver.app.customer.CustomerRepository;
+import ryver.app.stock.CustomStock;
 
-// import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-// import java.net.URI;
-// import java.util.LinkedHashMap;
-// import java.util.Optional;
+import java.net.URI;
+import java.util.ArrayList;
 
-// import org.junit.jupiter.api.AfterEach;
-// import org.junit.jupiter.api.BeforeAll;
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.TestInstance;
-// import org.junit.jupiter.api.TestInstance.Lifecycle;
 
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-// import org.springframework.boot.test.web.client.TestRestTemplate;
-// import org.springframework.boot.web.server.LocalServerPort;
-// import org.springframework.http.HttpEntity;
-// import org.springframework.http.HttpHeaders;
-// import org.springframework.http.HttpMethod;
-// import org.springframework.http.MediaType;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 
-// import net.minidev.json.JSONObject;
 
-// /** Start an actual HTTP server listening at a random port */
-// @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-// class StockIntegrationTest {
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-// 	@LocalServerPort
-// 	private int port;
+import net.minidev.json.JSONObject;
 
-// 	private final String baseUrl = "http://localhost:";
+/** Start an actual HTTP server listening at a random port */
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class StockIntegrationTest {
 
-// 	@Autowired
-// 	private TestRestTemplate restTemplate;
+	@LocalServerPort
+	private int port;
 
-// 	@Autowired
-//     private StockRepository stocks;
+	private final String baseUrl = "http://localhost:";
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Autowired
+    private StockRepository stocks;
+
+    @Autowired
+    private CustomerRepository customers;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+	@AfterEach
+	void tearDown() {
+        // clear the database after each test
+		stocks.deleteAll();
+    }
     
-//     @Autowired
-//     private CustomerRepository customers;
+    @Test
+    public void getStockBySymbol_ROLEUser_Success() throws Exception{
+        Customer customer = new Customer("user_1", encoder.encode("password"), "ROLE_USER", "user_fullname", "S7812345A", "91234567", "address", true);
+        customer.setId(1L);
+        customers.save(customer);
+        
+        CustomStock stock = new CustomStock(
+            "V03", 20.55, 20000, 20.60, 20000, 20.65, new ArrayList<Trade>());
+        stocks.save(stock);
 
-// 	@Autowired
-// 	private BCryptPasswordEncoder encoder;
+        URI postUri = new URI(baseUrl + port + "/stocks/" + stock.getSymbol());
 
-// 	@AfterEach
-// 	void tearDown() {
-//         // clear the database after each test
-//         customers.deleteAll();
-// 		stocks.deleteAll();
-//     }
+        ResponseEntity<CustomStock> result = restTemplate.withBasicAuth("user_1", "password").getForEntity(postUri, CustomStock.class);
+
+        assertEquals(200, result.getStatusCode().value());
+    }
     
-//     @Test
-//     public void getStockBySymbol_ROLEUser_Success() throws Exception{
-//         CustomStock stock = new CustomStock(
-//             "V03", 20.55, 20000, 20.60, 20000, 20.65, null);
-//         stocks.save(stock);
+    // @Test
+    // public void getStockBySymbol_ROLEUser_Failure() throws Exception{
+    //     URI uri = new URI(baseUrl + port + "/stocks/" + 1);
 
-//         URI postUri = new URI(baseUrl + port + "/stocks/" + stock.getSymbol());
-
-//         ResponseEntity<CustomStock> result = restTemplate.withBasicAuth("manager_1", "01_manager_01").getForEntity(postUri, CustomStock.class);
-
-//         assertEquals(201, result.getStatusCode().value());
-//     }
-    
-//     @Test
-//     public void getStockBySymbol_ROLEUser_Failure() throws Exception{
-//         URI uri = new URI(baseUrl + port + "/stocks/" + 1);
-
-// 		ResponseEntity<CustomStock> result = restTemplate.withBasicAuth("user_1", "password").getForEntity(uri, CustomStock.class);
+	// 	ResponseEntity<CustomStock> result = restTemplate.withBasicAuth("user_1", "password").getForEntity(uri, CustomStock.class);
 		
-// 		//requested trade not found
-// 		assertEquals(404, result.getStatusCode().value());
-//     }
-// }
+	// 	//requested trade not found
+	// 	assertEquals(404, result.getStatusCode().value());
+    // }
+}
