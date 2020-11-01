478375e48a4b3309db2df6502c41ff098cd46619
Jasmine <jasmine.tan.2019@sis.smu.edu.sg>
2020-11-01 14:08:49 +0800

    Update UserAlreadyExistsException

--- app/src/main/java/ryver/app/customer/UserAlreadyExistsException.java
+++ app/src/main/java/ryver/app/customer/UserAlreadyExistsException.java
@@ -3,7 +3,7 @@ package ryver.app.customer;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "User already exists. ")
 public class UserAlreadyExistsException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
