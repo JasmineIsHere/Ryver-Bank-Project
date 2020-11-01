ff62642a10d99ad0f78e77ddd0aa2dcb81b9c83d
Jen Leng <jen.leng.2019@sis.smu.edu.sg>
2020-11-01 17:41:00 +0800

    Javadoc comments - Customer

--- app/src/main/java/ryver/app/customer/CustomerMismatchException.java
+++ app/src/main/java/ryver/app/customer/CustomerMismatchException.java
@@ -3,9 +3,8 @@ package ryver.app.customer;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.FORBIDDEN, reason = "You cannot access this customer account")
+@ResponseStatus(code = HttpStatus.FORBIDDEN, reason = "You cannot access this customer account") // 403 Forbidden
 public class CustomerMismatchException extends RuntimeException{
-
     private static final long serialVersionUID = 1L;
 
     public CustomerMismatchException() {
--- app/src/main/java/ryver/app/customer/CustomerRepository.java
+++ app/src/main/java/ryver/app/customer/CustomerRepository.java
@@ -7,8 +7,19 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    // define a derived query to find user by username
+    /**
+     * Get a Customer, filtered by the specified username - can return null
+     * 
+     * @param username
+     * @return Optional<Customer>
+     */
     Optional<Customer> findByUsername(String username);
 
+    /**
+     * Get a List of Customers, filtered by the specified authorities
+     * 
+     * @param authorities
+     * @return List<Customer>
+     */
     List<Customer> findByAuthorities(String authorities);
 } 
