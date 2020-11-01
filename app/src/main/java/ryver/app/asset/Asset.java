fcd9588a15a2fa5d9d106f48f6a26bdbd6260bff
Jen Leng <jen.leng.2019@sis.smu.edu.sg>
2020-11-01 16:31:02 +0800

    Javadoc comments - Asset

--- app/src/main/java/ryver/app/asset/Asset.java
+++ app/src/main/java/ryver/app/asset/Asset.java
@@ -10,34 +10,74 @@ import ryver.app.util.jsonDoubleSerializer;
 
 import lombok.*;
 
+// Spring Annotations
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-public class Asset{
-    private  @Id @GeneratedValue (strategy = GenerationType.IDENTITY) Long id;
-    private String code; //symbol of stock eg. A17U
+
+public class Asset {
+    // Fields
+    /**
+     * The auto-generated ID for each account Starts from 1
+     */
+    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+    /**
+     * The symbol of the stock eg. A17U
+     */
+    private String code;
+
+    /**
+     * The quantity of stocks owned
+     */
     private int quantity;
-    
+
+    /**
+     * The average price of the stocks owned
+     */
     @JsonSerialize(using = jsonDoubleSerializer.class)
-    private double avg_price; 
-    
+    private double avg_price;
+
+    /**
+     * The current price of the stocks owned
+     */
     @JsonSerialize(using = jsonDoubleSerializer.class)
     private double current_price;
-    
+
+    /**
+     * The value of the stocks owned value = current_price * quantity
+     */
     @JsonSerialize(using = jsonDoubleSerializer.class)
-    private double value; 
-    
+    private double value;
+
+    /**
+     * The gain/loss of the stocks owned gain_loss = value - (avg_price * quantity)
+     */
     @JsonSerialize(using = jsonDoubleSerializer.class)
     private double gain_loss;
 
+    // Mappings
+    /**
+     * The ID of the associated Portfolio
+     */
     @ManyToOne
     @JsonIgnore
     @JoinColumn(name = "portfolio_id", nullable = false)
     private Portfolio portfolio;
 
+    // Constructors
+    /**
+     * Create an Asset with the specified parameters
+     * 
+     * @param code
+     * @param quantity
+     * @param avg_price
+     * @param current_price
+     * @param value
+     * @param gain_loss
+     */
     public Asset(String code, int quantity, double avg_price, double current_price, double value, double gain_loss) {
         this.code = code;
         this.quantity = quantity;
@@ -47,7 +87,19 @@ public class Asset{
         this.gain_loss = gain_loss;
     }
 
-    public Asset(String code, int quantity, double avg_price, double current_price, double value, double gain_loss, Portfolio portfolio) {
+    /**
+     * Create an Asset with the specified parameters (With associated Portfolio)
+     * 
+     * @param code
+     * @param quantity
+     * @param avg_price
+     * @param current_price
+     * @param value
+     * @param gain_loss
+     * @param portfolio
+     */
+    public Asset(String code, int quantity, double avg_price, double current_price, double value, double gain_loss,
+            Portfolio portfolio) {
         this.code = code;
         this.quantity = quantity;
         this.avg_price = avg_price;
@@ -57,8 +109,16 @@ public class Asset{
         this.portfolio = portfolio;
     }
 
-    @Override public String toString() {
-        String string = "Asset(assetid=" + this.id + ", code=" + this.code + ", quantity=" + this.quantity +  ", avg_price=" + this.avg_price +  ", current_price=" + this.current_price +  ", value=" + this.value +  ", gain_loss=" + this.gain_loss +")";
+    /**
+     * Convert Asset to String
+     * 
+     * @return String
+     */
+    @Override
+    public String toString() {
+        String string = "Asset(assetid=" + this.id + ", code=" + this.code + ", quantity=" + this.quantity
+                + ", avg_price=" + this.avg_price + ", current_price=" + this.current_price + ", value=" + this.value
+                + ", gain_loss=" + this.gain_loss + ")";
         return string;
     }
 }
